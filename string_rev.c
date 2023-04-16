03d6bba3899942869e3d1c275aba0a6d3012d72e
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-04-16 10:38:03 +0530

    added string_rev.c

---
+++ string_rev.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char ch[20];
+    printf("Enter String:");
+    gets(ch);
+    int count=0;
+    while(ch[count]!='\0'){
+        count++;
+    }
+    int i=0,j=count-1;
+    while(i<j){
+        char temp=ch[i];
+        ch[i]=ch[j];
+        ch[j]=temp;
+        i++;
+        j--;
+    }
+    printf("\nReversed string:%s\n\n",ch);
+    return 0;
+}
